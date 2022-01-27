15de99ce3085ff38a7b13b46ab390e8f7f9c93cd
jemin <rudanijemin18@gmail.com>
2022-01-27 17:59:22 +0530

    set temp

--- weather/index.js
+++ weather/index.js
@@ -4,6 +4,16 @@ var requests = require('requests');
 
 const homeFile = fs.readFileSync("home.html","utf-8");
 
+const replaceVal = (tempVal,orgVal) => {
+    let temperature = tempVal.replace("{%tempval%}",orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}",orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}",orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}",orgVal.name);
+    temperature = temperature.replace("{%country%}",orgVal.sys.country);
+    temperature = temperature.replace("{%tempStatus%}",orgVal.weather[0].main);
+    return temperature;
+};
+
 
 const server = http.createServer((req,res)=>{
     if(req.url == "/"){
@@ -13,13 +23,17 @@ const server = http.createServer((req,res)=>{
         .on('data',  (chunk) => {
             const objData = JSON.parse(chunk);
             const arrData = [objData];
-            console.log(arrData[0].main.temp-273.15);
+            //console.log(arrData[0].main.temp-273.15);
+            const realTimeDate = arrData.map(val =>replaceVal(homeFile,val)).join("");  //join arr formate ne string ma convert
+            res.write(realTimeDate);
+            //console.log(realTimeDate);
+                //  replaceVal(homeFile,val); 
+    
         })
         .on('end',  (err) => {
 
            if (err) return console.log('connection closed due to errors', err);
- 
-        console.log('end');
+           res.end();
         });
     }
 });
